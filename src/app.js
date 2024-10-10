793fe8096986a307b38cb0ffc1a3a43a5f1d2f8d
Juan Paladea <juanpaladea5@gmail.com>
2024-10-10 16:08:21 -0300

    Refactor app.js and add db.js for database connection

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
+const pool = require('./db');
 
 const app = express();
 
 app.use('/', (req, res) => {
-    res.send('Hello World');
+	res.send('Hello World');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000', 'http://localhost:3000');
+app.get('/users', async (req, res) => {
+	try {
+		const [rows] = await pool.query('SELECT * FROM users');
+		res.json(rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`, 'http://localhost:3000');
 }); 
