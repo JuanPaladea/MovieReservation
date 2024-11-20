3c0908e2566704c207070d246e4d3d34052b9e0a
Juan Paladea <juanpaladea5@gmail.com>
2024-11-20 16:12:15 -0300

    fix: Correct adminAuth middleware reference and enhance reservations API documentation; update movie rating type and add example for seatIds

--- src/routes/apiReservations.router.js
+++ src/routes/apiReservations.router.js
@@ -1,13 +1,13 @@
 const { addReservation, getReservations, getReservationById, getUserReservations, getShowtimeReservations, checkSeatAvailability, deleteReservation } = require('../controllers/reservationsController');
 const authToken = require('../middlewares/authToken');
-const admninAuth = require('../middlewares/adminAuth');
+const adminAuth = require('../middlewares/adminAuth');
 
 const router = require('express').Router();
 
-router.get('/', authToken, getReservations);
+router.get('/', authToken, adminAuth, getReservations);
 router.post('/', authToken, addReservation);
 router.get('/:reservationId', authToken, getReservationById);
-router.delete('/:reservationId', authToken, admninAuth, deleteReservation);
+router.delete('/:reservationId', authToken, adminAuth, deleteReservation);
 router.get('/user/:userId', authToken, getUserReservations);
 router.get('/showtime/:showtimeId', authToken, getShowtimeReservations);
 
